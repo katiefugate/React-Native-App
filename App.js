a735665843307ab7e4e8af502e9cc5a63298f2f7
katiefugate <katiiierochelle@yahoo.com>
2021-07-23 12:17:19 -0700

    just testing things out.

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import Home from './views/home.js';
+import Profile from './views/profile.js';
 
 export default function App() {
+  const [currentView, setCurrentView] = useState(< Home/>);
+
+  function homeTouch(event) {
+    setCurrentView(< Home />)
+  }
+
+  function profileTouch(event) {
+    setCurrentView(< Profile />)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Cat-a-Gram</Text>
+      {currentView}
+      <View style={styles.navbarContainer}>
+        <FontAwesome5 name="home" size={40} color="black" onPress={homeTouch} />
+        <FontAwesome5 name="user-circle" size={40} color="black" onPress={profileTouch} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +36,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navbarContainer: {
+    backgroundColor: 'purple',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0, padding: 25,
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  header: {
+    backgroundColor: 'purple',
+    width: '100%',
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingTop: 37,
+    paddingBottom: 15,
+    position: 'absolute',
+    top: 0, color: 'white',
+    textAlign: 'center'
+  }
 });
